d21ef88152f060d419fe870f5b82c74ef90bfbcb
ejamnadas <ejamnadas@datuma.io>
2022-11-05 13:37:19 -0700

    secuirty

--- pg_conn.py
+++ pg_conn.py
@@ -2,5 +2,5 @@
 import psycopg2
 
 def get_conn():
-    conn = psycopg2.connect(database="dp", user="ejamnadas", password="N33nte*", host="35.184.12.134", port="5432" )
+    conn = psycopg2.connect(database="dp", user="ejamnadas", password="xxxx*", host="xxxx", port="xxxx" )
     return conn
