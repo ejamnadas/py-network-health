1ffcd02a6eec75c57f80f9bc20f3bebdd5cdf4e5
ejamnadas <ejamnadas@datuma.io>
2022-12-01 10:32:05 -0800

    changes

--- main_min_since_last_hb.py
+++ main_min_since_last_hb.py
@@ -2,4 +2,7 @@ import dtma_ntw_health_tasks as tasks
 import variables as var
 
 #def lambda_handler(event, context):
-tasks.last_hb_in_min_fail(var.getProperty())
+tasks.last_hb_in_min_fail('LQS')
+tasks.last_hb_in_min_fail('LQR')
+#tasks.last_hb_in_min_fail(var.getProperty())
+#tasks.last_hb_in_min_fail(var.getProperty())
--- variables.py
+++ variables.py
@@ -1,6 +1,6 @@
-pgUser = 'xxx'
-pgWp = 'xxx'
-pgHost = 'xxx'
+pgUser = 'ejamnadas'
+pgWp = 'N33nte*'
+pgHost = '35.184.12.134'
 pgPort = '5432'
 
 def getProperty():
